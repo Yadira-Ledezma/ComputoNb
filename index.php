1d6fd058185497feefdefa144214f350fa28a609
Yadira-Ledezma <167580313+Yadira-Ledezma@users.noreply.github.com>
2024-10-14 08:05:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostrar Nombre</title>
+</head>
+<body>
+
+    <h1>Bienvenido</h1>
+    <p>Tu nombre es: 
+        <?php
+            $nombre = "YADIRA LEDEZMA ";
+            echo $nombre;
+        ?>
+    </p>
+
+</body>
+</html>
 
